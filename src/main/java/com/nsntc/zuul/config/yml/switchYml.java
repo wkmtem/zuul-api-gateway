2195103467ac5c71baa74483bdc0d5c9df31b27c
wkm <813272417@qq.com>
2017-12-30 17:43:04 +0800

    pre post error 过滤器

---
+++ src/main/java/com/nsntc/zuul/config/yml/switchYml.java
@@ -0,0 +1,28 @@
+package com.nsntc.zuul.config.yml;
+
+import lombok.Data;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+/**
+ * Class Name: switchYml
+ * Package: com.nsntc.zuul.config.yml
+ * Description: 全局开关
+ * @author wkm
+ * Create DateTime: 2017/12/30 下午5:35
+ * Version: 1.0
+ */
+@Data
+@Component
+@RefreshScope
+@SpringBootConfiguration
+@ConfigurationProperties(prefix = "global", ignoreUnknownFields = false)
+public class switchYml {
+
+    /**
+     * true: 开, false: 关
+     */
+    private Boolean switchVal;
+}
