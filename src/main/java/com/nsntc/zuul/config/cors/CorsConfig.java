2534629019729b67495b997b8a87fdb154e235ff
wkm <813272417@qq.com>
2018-01-12 19:54:55 +0800

    pre post error 过滤器

--- src/main/java/com/nsntc/zuul/config/cors/CorsConfig.java
+++ src/main/java/com/nsntc/zuul/config/cors/CorsConfig.java
@@ -1,12 +1,16 @@
 package com.nsntc.zuul.config.cors;
 
 
+import com.nsntc.commons.constant.CorsConstant;
+import com.nsntc.commons.enums.HttpMethodEnum;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -24,9 +28,14 @@ public class CorsConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
         /** 可自行筛选 */
-        corsConfiguration.addAllowedOrigin("*");
-        corsConfiguration.addAllowedHeader("*");
-        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin(CorsConstant.CORS_ALLOWED_ORIGIN);
+        corsConfiguration.addAllowedHeader(CorsConstant.CORS_ALLOWED_HEADER);
+        corsConfiguration.addAllowedMethod(HttpMethodEnum.GET.getCode());
+        corsConfiguration.addAllowedMethod(HttpMethodEnum.POST.getCode());
+        corsConfiguration.addAllowedMethod(HttpMethodEnum.PUT.getCode());
+        corsConfiguration.addAllowedMethod(HttpMethodEnum.DELETE.getCode());
+        corsConfiguration.addAllowedMethod(HttpMethodEnum.HEAD.getCode());
+        corsConfiguration.addAllowedMethod(HttpMethodEnum.OPTIONS.getCode());
 
         return corsConfiguration;
     }
@@ -34,22 +43,57 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig());
+        source.registerCorsConfiguration(CorsConstant.CORS_MAPPING_PATH, buildConfig());
         return new CorsFilter(source);
     }
 }
 
-/** 方式二 */
+/** 方法二: 能请求，返回有数据
+ *  提示 No 'Access-Control-Allow-Origin’ header is present on the requested resource.
+ *      Origin 'http://localhost:63342' is therefore not allowed.
+ */
 /*@SpringBootConfiguration
-public class CorsConfig extends WebMvcConfigurerAdapter {
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping(CorsConstant.CORS_MAPPING_PATH)
+                        .allowedOrigins(CorsConstant.CORS_ALLOWED_ORIGIN)
+                        .allowedHeaders(CorsConstant.CORS_ALLOWED_HEADER)
+                        .allowedMethods(HttpMethodEnum.GET.getCode(),
+                                HttpMethodEnum.POST.getCode(),
+                                HttpMethodEnum.PUT.getCode(),
+                                HttpMethodEnum.DELETE.getCode(),
+                                HttpMethodEnum.HEAD.getCode(),
+                                HttpMethodEnum.OPTIONS.getCode())
+                        *//** true: ajax请求需添加 xhrFields: {withCredentials: true} *//*
+                        .allowCredentials(false)
+                        .exposedHeaders(HttpHeaders.SET_COOKIE).maxAge(CorsConstant.CORS_MAX_AGE)
+                        .maxAge(CorsConstant.CORS_MAX_AGE);
+            }
+        };
+    }
+}*/
+
+
+/** 方法三: 能请求，返回有数据
+ *  提示 No 'Access-Control-Allow-Origin’ header is present on the requested resource.
+ *      Origin 'http://localhost:63342' is therefore not allowed.
+ */
+/*@SpringBootConfiguration
+public class Blog2MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowCredentials(true)
-                .allowedMethods("GET", "POST", "DELETE", "PUT")
-                .maxAge(3600);
+                .exposedHeaders(HttpHeaders.SET_COOKIE).maxAge(3600L)
+                .maxAge(3600L);
     }
 }*/
