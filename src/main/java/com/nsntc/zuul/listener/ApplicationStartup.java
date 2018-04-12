87158d9bd1fd38f0f350c30846d23b71e4a78a61
wkm <813272417@qq.com>
2018-04-12 16:23:39 +0800

    if(null == event.getApplicationContext().getParent()){}

--- src/main/java/com/nsntc/zuul/listener/ApplicationStartup.java
+++ src/main/java/com/nsntc/zuul/listener/ApplicationStartup.java
@@ -1,8 +1,7 @@
 package com.nsntc.zuul.listener;
 
+import com.nsntc.commons.abs.AbstractAfterSpringContextStarted;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 
 /**
@@ -14,27 +13,19 @@ import org.springframework.context.event.ContextRefreshedEvent;
  * Version: 1.0
  */
 @SpringBootConfiguration
-public class ApplicationStartup implements ApplicationListener<ContextRefreshedEvent> {
+public class ApplicationStartup extends AbstractAfterSpringContextStarted {
 
     /**
-     * ContextRefreshedEvent 当ApplicationContext或spring被初始化或者刷新initialized会触发该事件
-     * ContextStartedEvent	 spring初始化完时触发
-     * ContextStoppedEvent	 spring停止后触发，一个停止了的动作，可以通过start()方法从新启动
-     * ContextClosedEvent	 spring关闭，所有bean都被destroyed掉了,这个时候不能被刷新，或者从新启动了
-     * RequestHandledEvent	 请求经过DispatcherServlet时被触发，在request完成之后
-     * ApplicationReadyEvent 当spring mvc application应该初始化完成，可以准备接收请求
-     * StatusChangeEvent netflix 事件
+     * Class Name: ApplicationStartup
+     * Package: com.nsntc.zuul.listener
+     * Description:
+     * @author wkm
+     * Create DateTime: 2018/4/12 下午4:21
+     * Version: 1.0
      */
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        /**
-         * spring boot 默认情况下只有一个 WebApplicationContext
-         * org.springframework.boot.context.embedded.AnnotationConfigEmbeddedWebApplicationContext
-         * */
-        //if (event.getApplicationContext() instanceof AnnotationConfigEmbeddedWebApplicationContext) {
-        if(null == event.getApplicationContext().getParent()) {
-            /** 初始化菜单权限 */
-            //MenuPermissionUtils.initMenuPermission();
-        }
+    protected void applicationEvent(ContextRefreshedEvent event) {
+        /** 初始化菜单权限 */
+        //MenuPermissionUtils.initMenuPermission();
     }
 }
